1114018e0d22ab7bbbb1a7d147e0954396ae7d3a
birrustemof <gemg5685@gmail.com>
2025-11-16 18:26:16 +0400

    web.php faylında routları yenilədim

--- routes/web.php
+++ routes/web.php
@@ -10,21 +10,24 @@ Route::get('/', function () {
 
 // Xəbər route'ları
 Route::prefix('news')->name('news.')->group(function () {
-    Route::get('/', [NewsController::class, 'news'])->name('index');
-    Route::post('/store', [NewsController::class, 'store'])->name('store');
-    Route::delete('/{id}', [NewsController::class, 'destroy'])->name('destroy'); // YENİ
+    Route::get('/',                      [NewsController::class, 'news'])->name('index');
+    Route::post('/store',                [NewsController::class, 'store'])->name('store');
+    Route::delete('/{id}',               [NewsController::class, 'destroy'])->name('destroy');
+    Route::get('/deleted',               [NewsController::class, 'deletedNews'])->name('deleted');
+    Route::patch('/{id}/restore',        [NewsController::class, 'restore'])->name('restore');
+    Route::delete('/{id}/force-delete',  [NewsController::class, 'forceDelete'])->name('forceDelete');
+    Route::get('/export',                [NewsController::class, 'export'])->name('export'); // ƏLAVƏ EDİN
 });
 
 // Form route'ları - PageController istifadə edir
 Route::prefix('forms')->name('forms.')->group(function () {
-    Route::get('general/{id}', [NewsController::class, 'show'])->name('general');
-    Route::put('general/{id}', [NewsController::class, 'update'])->name('general.update');
+    Route::get('general/{id}',           [NewsController::class, 'show'])->name('general');
+    Route::put('general/{id}',           [NewsController::class, 'update'])->name('general.update');
 });
 
 // Digər route'lar
-Route::get("simple", [NewsController::class, 'news'])->name('simple');
-Route::get("", [PageController::class, 'index'])->name('index');
-Route::get("index2", [PageController::class, 'index2'])->name('index2');
-Route::get("index3", [PageController::class, 'index3'])->name('index3');
-Route::get('/xeber2', [PageController::class, 'xeber2'])->name('xeber2');
-
+Route::get("simple",                     [NewsController::class, 'news'])->name('simple');
+Route::get("",                           [PageController::class, 'index'])->name('index');
+Route::get("index2",                     [PageController::class, 'index2'])->name('index2');
+Route::get("index3",                     [PageController::class, 'index3'])->name('index3');
+Route::get('/xeber2',                    [PageController::class, 'xeber2'])->name('xeber2');
